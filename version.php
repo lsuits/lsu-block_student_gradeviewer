68b41b6890831b6db395aa9ccfe898d219f894d3
Chad Mazilly <chadmazilly@gmail.com>
2016-09-13 11:38:11 -0500

    bump ver

--- version.php
+++ version.php
@@ -1,4 +1,30 @@
 <?php
 
-$plugin->version = '2013062714';
-$plugin->requires = '2010112400';
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * @package   block_student_gradeviewer
+ * @copyright 2014, Louisiana State University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2016091300;
+$plugin->requires = 2013051400;
+$plugin->component = 'block_student_gradeviewer';
+$plugin->maturity = MATURITY_STABLE;
